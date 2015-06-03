cbf0d4b28484cb19b0df0d295babc7fc40613856
Iván Sánchez Ortega <ivan@mazemap.no>
2015-06-03 15:26:44 +0200

    First attempt at integrating eslint with the Gobble build system.

--- gobblefile.js
+++ gobblefile.js
@@ -1,4 +1,14 @@
 // gobblefile.js
 var gobble = require( 'gobble' );
 
-module.exports = gobble('src').transform('babel',{sourceMap: false});
+module.exports = gobble('src')
+	.transform('babel',{sourceMap: false})
+	.observe('eslint', {
+// 		env: {
+// 			es6: true
+// 		},
+		rules: {
+			// No special rules, let's use eslint's defaults by now.
+		}
+	})
+	;
